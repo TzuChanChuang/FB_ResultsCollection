bf5b656dbd84e85e71ffdb97949aa874e2ad92d2
TzuChanChuang <alice20041025@yahoo.com.tw>
2015-12-10 02:49:22 +0800

    fix post probelm

--- static/javascripts/publish.js
+++ static/javascripts/publish.js
@@ -1,50 +1,13 @@
 var Ques = '';
+var id = '';
 var check = 0;
+var sheetkey = '1K2v3VSv9xCrAmK5sTYFNwxE58DJ6K81xBRz6mYKsOeo';
+var url = 'https://spreadsheets.google.com/feeds/cells/'+ sheetkey +'/1/public/values?alt=json-in-script&callback=?';
 
 (function() {
 	$("#myQuestion").keypress(function(event){
     //alert("yo_1");
       	if(event.keyCode == 13){
-          //alert("yo_2");
-        	Ques = $("#myQuestion").val();
-        	if(check==1){
-            //alert("yo_3");
-        		postQuestion();
-        	}else{
-        		alert('Pleaseeeeee log into Facebook.')
-        	}	
         }      
   	});
 })();
-
-function postQuestion() {
-	alert("post!");
-  	FB.api('/me/feed', 'post', {
-      message:Ques       
-    }, function(response){
-   		if (!response || response.error) {
-        	alert('Error occured');
-    	} else {
-        	console.log('Post ID: ' + response.id);
-    	}
-  	});
-}
-
-function checkLoginStateyo() {
-  //alert("yes_1");
-  	FB.getLoginStatus(function(response) {
-  	  	statusChangeCallbackyo(response);
-  	});
-}
-
-function statusChangeCallbackyo(response) {
-  //alert("yes_2");
-    if (response.status === 'connected') {
-      	check=1;
-    } else if (response.status === 'not_authorized') {
-      alert('Please log into this app.');
-    } else {
-      alert('Please log into Facebook.')
-  	}
-}
-
