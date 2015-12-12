ba4cf5d109287ad52c042b9e9355b2b959edb6fb
TzuChanChuang <alice20041025@yahoo.com.tw>
2015-12-12 19:29:32 +0800

    finish result_content page, able to show comments and likes, and sort the number of likes

--- static/javascripts/result.js
+++ static/javascripts/result.js
@@ -18,20 +18,20 @@ function getPost() {
     var post_now = 0;
     // Create a deferred object
     var dfd = $.Deferred();
-    console.log('ques_id.length = '+ ques_id.length);
     for(var i=0; i<ques_id.length; i++){
         (function (i) {
             FB.getLoginStatus(function(response) {
                 FB.api('/' + ques_id[i] + '?fields=from,message', function(response) {
                     if (response && !response.error) {
-                        console.log(response);
+                        
                         posts.push({});
                         posts[post_now].post = response.message;
+                        posts[post_now].name = response.from.name;
+                        console.log(posts[post_now].name);
                         posts[post_now].id = ques_id[i];
                         post_now = post_now + 1;
                     }else{
                     }
-                    console.log(posts.length);
                     if(ques_id.length==posts.length){
                         dfd.resolve();
                     }
@@ -44,7 +44,7 @@ function getPost() {
         console.log(posts.length);
         for(var i=0; i<posts.length; i++){
             var link = "/result/" + posts[i].id;
-            document.getElementById("posts").innerHTML = document.getElementById("posts").innerHTML + '<a href="' + link + '" ><p class="borderexample" style="width: 40%;">' + posts[i].post + '</p></a></div>';
+            document.getElementById("posts").innerHTML = document.getElementById("posts").innerHTML + '<a href="' + link + '" ><p class="borderexample" style="width: 40%; margin:0px auto;">' + posts[i].name+': '+posts[i].post + '</p></a>';
         }
     });
 }
